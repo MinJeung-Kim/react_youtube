85136c810b75a93cc41c95e238b23f23e552f1c3
MinJeung-Kim <focso5@knou.ac.kr>
2021-05-14 16:06:17 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
-    // 개발자도구(F12) => Components Tab => 값 확인
-         <h1>{props.video.snippet.title}</h1>
+/* 
+> deconstructiong
+     props.video === video
+     props.video.snippet === {video: {snippet}}
+*/
+const VideoItem = ({video: {snippet}}) => (
+    // 개발자도구(F12) => Components Tab => 값 확인 
+         <li className={styles.container}>
+             <div className={styles.video}>
+                 {/* 이미지 출력 */}
+                <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+                <div className={styles.metadata}>
+                    <p className={styles.title}>{snippet.title}</p>
+                    <p className={styles.channel}>{snippet.channelTitle}</p>
+                </div>
+             </div> 
+         </li>
     );
 
 export default VideoItem;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from  './video_list.module.css'
 
 const VideoList = (props) => (
-            <ul>
+            <ul className={styles.videos}>
                 {/* VideoItem컴포넌트에 video를 전달 */}
                 {props.videos.map(video => <VideoItem key={video.id} video={video} />)}
             </ul>
