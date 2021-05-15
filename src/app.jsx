e2e3ce90bd9f5b9fdd1152f54a4372d6f9817099
MinJeung-Kim <focso5@knou.ac.kr>
2021-05-15 16:59:41 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from 'react';
-import './app.css'; 
+import styles from './app.module.css'; 
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {
   // 1. 유튜브 데이터를 받아오기
 
   //video목록 저장 및 업데이트
   const [videos, setVideos] = useState([]);
 
+  const search = (query) => {
+    youtube.search(query)
+    .then(videos => setVideos(videos));
+  }
+
   //마운트, 업데이트가 됐을때 콜백함수 호출
   //두번째 인자 [] => 업데이트 될때마다 통신 지양, 마운트가 되었을때 한번만 콜백 함수 호출되도록 설정
   useEffect(() => {
-    //포스트맨 => javascript - Fetch(기본)
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyD7fs3ZE22OJljZE6v0J9ZQrAyec1oL8lU&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD7fs3ZE22OJljZE6v0J9ZQrAyec1oL8lU", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import styles from './search_header.module.css';
+
+// 검색 이벤트가 발생하면 onSearch 콜백 함수 호출
+const SearchHeader = ({onSearch}) => {
+    const inputRef = useRef();
+    const handleSearch = () => {
+        // input태그의 현재 값 가져오기
+        const value = inputRef.current.value;
+        onSearch(value);
+    }
+    const onClick = () => {
+        handleSearch();
+    };
+
+    const onKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            handleSearch();
+        } 
+    };
+        return (
+            <header className={styles.header}>
+                <div className={styles.logo}>
+                    <img className={styles.img} src="/images/logo.png" alt="logo" />
+                    <h1 className={styles.title}>Youtube</h1>
+                </div> 
+                <input ref={inputRef} className={styles.input} type="search" placeholder="Search..." onKeyPress={onKeyPress}/>
+                <button className={styles.button} type="submit" onClick={onClick}>
+                    <img className={styles.buttonImg} src="/images/search.png" alt="search" />
+                </button>
+            </header>
+        );
+    };
+
+export default SearchHeader;
